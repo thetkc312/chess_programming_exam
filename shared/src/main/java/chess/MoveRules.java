3a278f002538f83ba8d4008ecd0167a9a36ea355
Paul Atreides <thetkc312@gmail.com>
2025-09-23 21:53:52 -0600

    Implementation of isValidMove and isEnemyInPosition for MoveRules

--- shared/src/main/java/chess/MoveRules.java
+++ shared/src/main/java/chess/MoveRules.java
@@ -6,9 +6,31 @@ abstract class MoveRules {
 
     final boolean unlimitedMovement;
 
-    abstract HashSet<int[]> getPotentialMoves(ChessBoard boardState, ChessPosition activePosition);
+    abstract HashSet<Movement> getPotentialMovements(ChessBoard boardState, ChessPosition activePosition);
 
     MoveRules(boolean unlimitedMovement) {
         this.unlimitedMovement = unlimitedMovement;
+    }
+
+    public boolean isValidMove(ChessBoard board, ChessPosition activePosition, ChessPosition targetPosition) {
+        if (!targetPosition.isOnBoard()) {
+            return false;
+        }
+        if (board.getPiece(targetPosition) == null) {
+            return true;
+        }
+        if (isEnemyInPosition(board, activePosition, targetPosition)) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isEnemyInPosition(ChessBoard board, ChessPosition activePosition, ChessPosition targetPosition) {
+        ChessPiece activePiece = board.getPiece(activePosition);
+        ChessPiece targetPiece = board.getPiece(targetPosition);
+        if (targetPiece == null) {
+            return false;
+        }
+        return activePiece.getTeamColor() != targetPiece.getTeamColor();
     }
 }
