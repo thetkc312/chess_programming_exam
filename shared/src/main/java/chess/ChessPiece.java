511df1025ea8c0826d12bf212eac52f680a4ac17
Paul Atreides <thetkc312@gmail.com>
2025-09-23 21:03:03 -0600

    Implementations of toChar and getFromChar factory methods, where getFromChar is a factory constructor, and toChar is just a method that can be called before initializing a part.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -19,6 +19,55 @@ public class ChessPiece {
         this.type = type;
     }
 
+    public ChessPiece getFromChar(char c) {
+        ChessPiece.PieceType checkPieceType = switch (c) {
+            case 'r' -> PieceType.ROOK;
+            case 'R' -> PieceType.ROOK;
+            case 'n' -> PieceType.KNIGHT;
+            case 'N' -> PieceType.KNIGHT;
+            case 'b' -> PieceType.BISHOP;
+            case 'B' -> PieceType.BISHOP;
+            case 'q' -> PieceType.QUEEN;
+            case 'Q' -> PieceType.QUEEN;
+            case 'k' -> PieceType.KING;
+            case 'K' -> PieceType.KING;
+            case 'p' -> PieceType.PAWN;
+            case 'P' -> PieceType.PAWN;
+            default -> null;
+        };
+        if (checkPieceType == null) {
+            return null;
+        } else {
+            ChessGame.TeamColor checkPieceColor;
+            if (Character.isLowerCase(c)) {
+                checkPieceColor = ChessGame.TeamColor.BLACK;
+            } else {
+                checkPieceColor = ChessGame.TeamColor.WHITE;
+            }
+            return new ChessPiece(checkPieceColor, checkPieceType);
+        }
+    }
+
+    // Factory method to take a ChessPiece object as a parameter and determine the appropriate character to represent it
+    // (importantly, it is static and called on the class, with an instance of the class passed to it).
+    public static char toChar(ChessPiece myPiece) {
+        if (myPiece == null) {
+            return '-';
+        }
+        char c = switch (myPiece.getPieceType()) {
+            case PieceType.ROOK -> 'r';
+            case PieceType.KNIGHT -> 'n';
+            case PieceType.BISHOP -> 'b';
+            case PieceType.QUEEN -> 'q';
+            case PieceType.KING -> 'k';
+            case PieceType.PAWN -> 'p';
+        };
+        if (myPiece.getTeamColor() == ChessGame.TeamColor.WHITE) {
+            c = Character.toUpperCase(c);
+        }
+        return c;
+    }
+
     /**
      * The various different chess piece options
      */
